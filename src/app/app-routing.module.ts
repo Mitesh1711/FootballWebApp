62ed8dd4f8b700e86fcdce334165329e709e8f17
Mitesh Patel <mitesh1711@ymail.com>
2019-02-07 12:48:02 +1100

    League table and results components seperated with routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LeagueTable} from './league-table';
+import {ResultsComponent} from './results/results.component';
+import {LeagueTableComponent} from './league-table/league-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LeagueTableComponent },
+  { path: 'results', component: ResultsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
